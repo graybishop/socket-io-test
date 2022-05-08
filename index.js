80171bdf935bbc9197b1b373766953808ac00741
Jake Swackhammer <j.100101@gmail.com>
2022-05-08 11:16:33 -0400

    added serverside logic to start socket, and clientside to init socket

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from 'express';
 const app = express();
 import { createServer } from 'http';
 const server = createServer(app);
+import { Server } from 'socket.io'
+const io = new Server(server);
 
 // workaround for __dirname in ESM modules
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +14,13 @@ const __dirname = dirname(__filename);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket)=>{
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
