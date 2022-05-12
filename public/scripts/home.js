0df206b1aaea97a5c45d65c217e16454cec15228
Jake Swackhammer <j.100101@gmail.com>
2022-05-11 20:00:35 -0400

    gave the 'and' its own style

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -6,25 +6,26 @@ const button = document.getElementById('submitButton');
 const appendNewMessage = (msgText, msgColor) => {
   const item = document.createElement('li');
   item.textContent = msgText;
-  if (msgColor){
-    item.style.color = msgColor
+  if (msgColor) {
+    item.style.color = msgColor;
   }
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
-}
+};
 
 const appendUserList = (userCount, users) => {
   const item = document.createElement('li');
   item.textContent = `A user has connected. Current User Count: ${userCount}. Users: `;
   messages.appendChild(item);
-  users.forEach(({nickname, userColor}, index) => {
-    const span = document.createElement('span')
-    span.style.color = userColor
+  users.forEach(({ nickname, userColor }, index) => {
+    const span = document.createElement('span');
+    span.style.color = userColor;
+    span.style.fontWeight = 'bold';
     index != users.length - 1 ?
       span.textContent = `${nickname}, `
       :
-      span.textContent = `and ${nickname}.`;
-      item.appendChild(span)
+      span.innerHTML = `<span style='font-weight:normal; color:initial'>and</span> ${nickname}.`;
+    item.appendChild(span);
   });
   window.scrollTo(0, document.body.scrollHeight);
 };
@@ -34,7 +35,7 @@ const formatUserList = (userListArray) => {
     return;
   }
   let formattedString = '';
-  userListArray.forEach(({nickname, userColor}, index) => {
+  userListArray.forEach(({ nickname, userColor }, index) => {
     //Add commas between each nickname, on last entry add an 'and' before the user
     index != userListArray.length - 1 ?
       formattedString = formattedString + `<span style='color:${userColor}'>${nickname},</span>`
@@ -44,14 +45,14 @@ const formatUserList = (userListArray) => {
   return formattedString;
 };
 
-const generateColor = ()=>{
+const generateColor = () => {
   const letters = '0123456789ABCDEF'.split('');
   let color = '#';
-  for (let i = 0; i < 6; i++ ) {
-      color += letters[Math.round(Math.random() * 15)];
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
   }
   return color;
-}
+};
 
 let nickname = '';
 if (!nickname) {
@@ -64,25 +65,25 @@ form.addEventListener('submit', (e) => {
 
   if (input.value && !nickname) {
     nickname = input.value;
-    userColor = generateColor()
+    userColor = generateColor();
     appendNewMessage(`Welcome to the chat ${nickname}.`, userColor);
-    socket.emit('user created', {nickname, userColor});
+    socket.emit('user created', { nickname, userColor });
     button.textContent = 'Send';
     input.value = '';
   }
 
   if (input.value && nickname) {
-    socket.emit('chat message', input.value, {nickname, userColor});
+    socket.emit('chat message', input.value, { nickname, userColor });
     input.value = '';
   }
 });
 
-socket.on('chat message', (msg, {nickname, userColor}) => {
+socket.on('chat message', (msg, { nickname, userColor }) => {
   appendNewMessage(`${nickname}: ${msg}`, userColor);
 });
 
 socket.on('user connected', (userCount, users) => {
-  appendUserList(userCount, users)
+  appendUserList(userCount, users);
 });
 
 socket.on('user disconnected', (userCount) => {
